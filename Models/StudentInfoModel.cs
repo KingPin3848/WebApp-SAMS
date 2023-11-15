a7deadd3f756d7a5187e0f3847acc6f12841b5f5
Shivam Vyas <Shivam_Vyas2006@hotmail.com>
2023-11-15 11:13:25 -0500

    Added the studentInfo and teacherInfo models to the Models folder, for now. Will add the rest of the models either today or tomorrow.

---
+++ Models/StudentInfoModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SAMS.Models
+{
+    public class StudentInfoModel
+    {
+        protected int studentID { get; private set; }
+        protected string? studentFirstNameMod { get; private set; }
+        protected string? studentMiddleNameMod { get; private set; }
+        protected string? studentLastNameMod { get; private set; }
+        protected string? studentPreferredNameMod { get; private set; }
+        protected string? studentEmailMod { get; private set; }
+        protected string? studentPhoneMod { get; private set; }
+        protected DateOnly? studentGradYearMod { get; private set; }
+        protected string? studentCounselorNameMod { get; private set; }
+        protected string? studentCounselorEmailMod { get; private set; }
+        protected string? studentEANameMod { get; private set; }
+        protected string? studentEAEmailMod { get; private set; }
+        protected string? parentguard1NameMod { get; private set; }
+        protected string? parentguard1EmailMod { get; private set; }
+        protected string? parentguard2NameMod { get; private set; }
+        protected string? parentguard2EmailMod { get; private set; }
+        //WE NEED TO FIND OUT HOW WE CAN INCLUDE IMAGES IN THIS MODEL. EITHER WE STORE IT IN THE
+        //DATABASE AND DIRECTLY ACCESS IT IN THE VIEW OR DO IT SOMEWAY OTHER.
+        //OR GET IT FROM THEIR GOOGLE ACCOUNT.
+    }
+}
---
+++ Models/TeacherInfoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SAMS.Models
+{
+    public class TeacherInfoModel
+    {
+        protected string? teacherID { get; private set; }
+        protected string? teacherFirstNameMod { get; private set; }
+        protected string? teacherMiddleNameMod { get; private set; }
+        protected string? teacherLastNameMod { get; private set; }
+        protected string? teacherPreferredNameMod { get; private set; }
+        protected string? teacherEmailMod { get; private set; }
+        protected string? teacherPhoneMod { get; private set; }
+    }
+}
