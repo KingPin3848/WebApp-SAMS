bcf5b9e3490866d31d800c79c5d4db8621b56a06
Shivam Vyas <Shivam_Vyas2006@hotmail.com>
2023-11-16 11:00:10 -0500

    Fixed the "?" problem.

--- Models/DailyBellScheduleModel.cs
+++ Models/DailyBellScheduleModel.cs
@@ -3,8 +3,8 @@
     public class DailyBellScheduleModel
     {
         public string? BellName {get;}
-        public TimeSpan StartTime {get;}
+        public TimeSpan? StartTime {get;}
         public TimeSpan? EndTime {get;}
-        public TimeSpan Duration {get;}
+        public TimeSpan? Duration {get;}
     }
 }
