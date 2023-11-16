2ddd7c31ec604bef1e6f6ccb12bd14d2601707c6
Shivam Vyas <Shivam_Vyas2006@hotmail.com>
2023-11-16 09:56:56 -0500

    Just added the daily bell schedule model. I will change it later. DO NOT WORK ON THIS YET!

---
+++ Models/DailyBellScheduleModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SAMS.Models
+{
+    public class DailyBellScheduleModel
+    {
+        public string? BellName { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan? EndTime { get; set;}
+        public TimeSpan Duration
+        {
+            get
+            {
+                return (TimeSpan)(EndTime - StartTime);
+            }
+        }
+    }
+}
