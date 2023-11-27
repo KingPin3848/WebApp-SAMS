a0e1f8ab65648c716e62e942033f7f2f95c9743e
Shivam Vyas <Shivam_Vyas2006@hotmail.com>
2023-11-27 12:22:43 -0500

    Made the applicationdbcontext nullable for now so that there are no nullable reference errors.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,33 +10,31 @@ namespace SAMS.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
             : base(options)
         {
         }
 
-        public DbSet<ActiveCourseInfoModel> activeCourseInfoModels { get; set; }
-        public DbSet<AdminInfoModel> adminInfoModels { get; set; }
-        public DbSet<AttendanceOfficeMemberModel> attendanceOfficeMemberModels { get; set; }
-        public DbSet<DailyBellScheduleModel> dailyBellScheduleModels { get; set; }
-        public DbSet<DeveloperInfoModel> developerInfoModels { get; set; }
-        public DbSet<EASuportInfoModel> eASuportInfoModels { get; set; }
-        public DbSet<ExtendedAvesBellScheduleModel> extendedAvesModels { get; set; }
-        public DbSet<FastPassModel> fastPassModels { get; set; }
-        public DbSet<HallPassInfoModel> hallPassInfoModels { get; set; }
-        public DbSet<LawEnforcementInfoModel> lawEnforcementInfoModels { get; set; }
-        public DbSet<NurseInfoModel> nurseInfoModels { get; set; }
-        public DbSet<PassRequestInfoModel> passRequestInfoModels { get; set; }
-        public DbSet<PepRallyBellScheduleModel> pepRallyBellScheduleModels { get; set; }
-        public DbSet<RoomLocationInfoModel> roomLocationInfoModels { get; set; }
-        public DbSet<StudentInfoModel> studentInfoModels { get; set; }
-        public DbSet<StudentScheduleInfoModel> studentScheduleInfoModels { get; set; }
-        public DbSet<SubstituteInfoModel> substituteInfoModels { get; set; }
-        public DbSet<SynnLabQRNodeModel> synnLabQRNodeModels { get; set; }
-        public DbSet<TeacherInfoModel> teacherInfoModels { get; set; }
-        public DbSet<TwoHrDelayBellScheduleModel> twoHrDelayBellScheduleModels { get; set; }
+        public DbSet<ActiveCourseInfoModel> activeCourseInfoModels { get; set; } = null!;
+        public DbSet<AdminInfoModel> adminInfoModels { get; set; } = null!;
+        public DbSet<AttendanceOfficeMemberModel> attendanceOfficeMemberModels { get; set; } = null!;
+        public DbSet<DailyBellScheduleModel> dailyBellScheduleModels { get; set; } = null!;
+        public DbSet<DeveloperInfoModel> developerInfoModels { get; set; } = null!;
+        public DbSet<EASuportInfoModel> eASuportInfoModels { get; set; } = null!;
+        public DbSet<ExtendedAvesBellScheduleModel> extendedAvesModels { get; set; } = null!;
+        public DbSet<FastPassModel> fastPassModels { get; set; } = null!;
+        public DbSet<HallPassInfoModel> hallPassInfoModels { get; set; } = null!;
+        public DbSet<LawEnforcementInfoModel> lawEnforcementInfoModels { get; set; } = null!;
+        public DbSet<NurseInfoModel> nurseInfoModels { get; set; } = null!;
+        public DbSet<PassRequestInfoModel> passRequestInfoModels { get; set; } = null!;
+        public DbSet<PepRallyBellScheduleModel> pepRallyBellScheduleModels { get; set; } = null!;
+        public DbSet<RoomLocationInfoModel> roomLocationInfoModels { get; set; } = null!;
+        public DbSet<StudentInfoModel> studentInfoModels { get; set; } = null!;
+        public DbSet<StudentScheduleInfoModel> studentScheduleInfoModels { get; set; } = null!;
+        public DbSet<SubstituteInfoModel> substituteInfoModels { get; set; } = null!;
+        public DbSet<SynnLabQRNodeModel> synnLabQRNodeModels { get; set; } = null!;
+        public DbSet<TeacherInfoModel> teacherInfoModels { get; set; } = null!;
+        public DbSet<TwoHrDelayBellScheduleModel> twoHrDelayBellScheduleModels { get; set; } = null!;
 
     }
 }
