fb827ee8ce545c698cc6cdcb5d4c82d6a73f1c64
Shivam Vyas <Shivam_Vyas2006@hotmail.com>
2023-12-12 14:12:08 -0500

    Nothing. Just made a few changes but it did not work out. Will try more once again later.

---
+++ Models/ActivationModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace SAMS.Models
+{
+    public class ActivationModel
+    {
+        [Required]
+        [Display(Name = "Activation Code")]
+        [Key]
+        public string ActivationCode { get; set; } = string.Empty;
+        [Display(Name = "Enable User Experience")]
+        public bool EnableUserExperience { get; set; }
+    }
+}
